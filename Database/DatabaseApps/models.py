6575350ffdb781e6bb4354ed5f16ce63a6319cd8
Lukas <67201471+LukasKuzel@users.noreply.github.com>
2022-10-12 22:04:32 +0200

    Database_Start

---
+++ Database/DatabaseApps/models.py
@@ -0,0 +1,83 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+
+class Kind(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    surname = models.CharField(max_length=250, unique=True)
+    birthdate = models.DateField
+    email = models.CharField(max_length=250, null=False)
+    phone_number = models.IntegerField(null=False)
+    password = models.CharField(max_length=250, unique=True, null=False)
+    city = models.CharField(max_length=250, null=False)
+    street = models.CharField(max_length=250, null=False)
+    PCS = models.CharField(max_length=250, null=True)
+    timestamp = models.DateTimeField
+
+    def __str__(self):
+        return self.name
+
+
+class Quiz_answer(models.Model):
+    answer = models.CharField(max_length=250, verbose_name="Answer")
+    verify = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(1)], null=False, verbose_name="0-false or 1-true")
+
+
+class Quiz_question(models.Model):
+    question = models.CharField(max_length=250, verbose_name="Question")
+    quizAnswer = models.ManyToManyField(Quiz_answer)
+
+
+class Quiz(models.Model):
+    quizQuestion = models.ManyToManyField(Quiz_question)
+    profile = models.ManyToManyField(Profile)
+
+    def __str__(self):
+        return self.quizQuestion
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=250, unique=True, verbose_name="Name")
+    surname = models.CharField(max_length=250, unique=True, verbose_name="Surname")
+    birthdate = models.DateField
+    photo = models.ImageField(blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    quiz = models.ManyToManyField(Quiz)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=250, unique=True, verbose_name="Title")
+    rate = models.FloatField(default=0, validators=[MinValueValidator(1), MaxValueValidator(100)], blank=True,
+                             null=True, help_text="Enter between 1 - 100")
+    photo = models.ImageField(blank=True, null=True)
+    plot = models.TextField(blank=True, null=True)
+    quiz = models.ManyToManyField(Quiz)
+    kind = models.ManyToManyField(Kind)
+    author = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    text = models.TextField
+    rate = models.FloatField(default=0, validators=[MinValueValidator(1), MaxValueValidator(5)], blank=True,
+                             null=True, help_text="Enter between 1 - 5")
+    profile = models.ManyToManyField(Profile)
+    book = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.text
+
+
+
