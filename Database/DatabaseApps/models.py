3af658d41324bf39dc53f270bf34c642c15bea18
Lukas <67201471+LukasKuzel@users.noreply.github.com>
2022-11-05 21:12:57 +0100

    Ukládání Fotek

--- Database/DatabaseApps/models.py
+++ Database/DatabaseApps/models.py
@@ -2,6 +2,14 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
+def poster_book_path(instance, filename):
+    return "book/" + str(instance.id) + "/book_poster/" + filename
+
+
+def poster_author_path(instance, filename):
+    return "author/" + str(instance.id) + "/author_poster/" + filename
+
+
 class Genre(models.Model):
     name = models.CharField(max_length=60, unique=True)
 
@@ -52,7 +60,7 @@ class Author(models.Model):
     surname = models.CharField(max_length=250, unique=False, verbose_name="Surname")
     nation = models.CharField(max_length=100, verbose_name="Nation", null=True)
     birthdate = models.CharField(max_length=100, verbose_name="Age", null=True)
-    photo = models.ImageField(blank=True, null=True)
+    photo = models.ImageField(upload_to=poster_author_path, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
 
     class Meta:
@@ -62,14 +70,12 @@ class Author(models.Model):
         return f'{self.name} {self.surname}'
 
 
-
-
 class Book(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=250, verbose_name="Title")
     rate = models.FloatField(default=0, validators=[MinValueValidator(1), MaxValueValidator(100)], blank=True,
                              null=True, help_text="Enter between 1 - 100")
-    photo = models.ImageField(blank=True, null=True)
+    photo = models.ImageField(upload_to=poster_book_path, blank=True, null=True)
     plot = models.TextField(blank=True, null=True)
     genre = models.ManyToManyField(Genre)
     author = models.ManyToManyField(Author)
@@ -91,6 +97,7 @@ class Review(models.Model):
 
     def __str__(self):
         return self.text
+
 
 
 
