e4a822e6ad5ced95b9b31d85b08bf3b16cc65bb1
Lukas <67201471+LukasKuzel@users.noreply.github.com>
2022-11-25 17:00:57 +0100

    Login/Register Again

--- Database/DatabaseAccounts/forms.py
+++ Database/DatabaseAccounts/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Profile
 
 
 class CreateUserForm(UserCreationForm):
@@ -11,3 +12,9 @@ class CreateUserForm(UserCreationForm):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} {self.email}'
+
+
+class CreateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['city']
--- Database/DatabaseAccounts/models.py
+++ Database/DatabaseAccounts/models.py
@@ -1,21 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=250)
-    surname = models.CharField(max_length=250)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     birthdate = models.DateField
-    email = models.CharField(max_length=250, null=False, unique=True)
-    phone_number = models.IntegerField(null=False)
-    password = models.CharField(max_length=250, null=False)
+    phone_number = models.CharField(max_length=12,null=False)
     city = models.CharField(max_length=250, null=False)
     street = models.CharField(max_length=250, null=False)
     PCS = models.CharField(max_length=250, null=True)
     timestamp = models.DateTimeField
 
-    class Meta:
-        ordering = ['name']
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
-    def __str__(self):
-        return f'{self.name} {self.surname} {self.email}'
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
